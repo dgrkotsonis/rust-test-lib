aff094739e917a0a192a889841fec9dca20fc1df
Gabe K <dgrkotsonis@gmail.com>
2025-02-17 21:16:01 -0500

    Basic lib for testing

---
+++ basic_math_lib/src/lib.rs
@@ -0,0 +1,21 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+pub fn multiply(left: u64, right: u64) -> u64 {
+    left * right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+
+        let result2 = multiply(2, 6);
+        assert_eq!(result2, 12);
+    }
+}
