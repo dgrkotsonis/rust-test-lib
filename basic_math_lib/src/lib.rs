1d189b184eddc58c3ec18731e447adbc4964f0f0
Gabe K <dgrkotsonis@gmail.com>
2025-02-21 14:47:47 -0500

    Add Square

--- basic_math_lib/src/lib.rs
+++ basic_math_lib/src/lib.rs
@@ -14,6 +14,10 @@ pub fn divide(left: u64, right: u64) -> u64 {
     left / right
 }
 
+pub fn square(num: u64) -> u64 {
+    num * num
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -31,5 +35,8 @@ mod tests {
 
         let result4 = subtract(14, 3);
         assert_eq!(result4, 11);
+
+        let result5 = square(7);
+        assert_eq!(result5, 49);
     }
 }
